26ae53bebb885d355c0a7bf8a29ac202840a9a41
Gustav Berg <gurrabe@gmail.com>
2021-11-24 09:28:40 +0000

    update

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const bodyParser = require("body-parser");
 const morgan = require('morgan');
 const cors = require('cors');
-var bcrypt = require('bcryptjs');
 
 const db = require("./db/database.js");
 const port = 1337;
@@ -51,124 +50,6 @@ app.get("/", (req, res) => {
     });
 });
 
-app.get("/reports/week/:id", (req, res) => {
-    let id = parseInt(req.params.id) + 1;
-    let sql = "SELECT heading, content FROM texts WHERE id = ?";
-
-    db.get(sql, [id], (err, row) => {
-        if (err) {
-            return res.status(403).json({
-                data: {
-                    msg: 'text failed'
-                }
-            });
-        }
-        if (row) {
-            return res.status(201).json({
-                data: {
-                    text: row
-                }
-            });
-        }
-        return res.status(403).json({
-            data: {
-                msg: 'text failed'
-            }
-        });
-    });
-});
-
-app.get("/admin", (req, res) => {
-    let sql = "SELECT * FROM texts";
-
-    db.all(sql, (err, row) => {
-        if (err) {
-            return res.status(403).json({
-                data: {
-                    msg: 'text failed'
-                }
-            });
-        }
-        if (row) {
-            return res.status(201).json({
-                data: {
-                    text: row
-                }
-            });
-        }
-        return res.status(403).json({
-            data: {
-                msg: 'text failed'
-            }
-        });
-    });
-});
-
-app.get("/admin/edit/:id", (req, res) => {
-    let sql = "SELECT id, heading, content FROM texts WHERE id = ?";
-
-    db.get(sql, [req.params.id], (err, row) => {
-        if (err) {
-            return res.status(403).json({
-                data: {
-                    msg: 'text failed'
-                }
-            });
-        }
-        if (row) {
-            return res.status(201).json({
-                data: {
-                    text: row
-                }
-            });
-        }
-        return res.status(403).json({
-            data: {
-                msg: 'text failed'
-            }
-        });
-    });
-});
-
-app.post("/register", (req, res) => {
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(req.body.psw, salt, function(err, hash) {
-            db.run("INSERT INTO users (email, password) VALUES (?, ?)",
-                req.body.usr,
-                hash, (err) => {
-                    if (err) {
-                        return res.status(403).json({
-                            data: {
-                                msg: 'failed'
-                            }
-                        });
-                    }
-                    return res.status(201).json({
-                        data: {
-                            msg: 'success'
-                        }
-                    });
-                });
-        });
-    });
-});
-
-app.post("/login", (req, res) => {
-    checkLogin(req, res);
-});
-
-app.post("/reports",
-    (req, res, next) => checkToken(req, res, next),
-    (req, res) => addReport(res, req.body));
-
-app.post("/edit",
-    (req, res, next) => checkToken(req, res, next),
-    (req, res) => editReport(res, req.body));
-
-app.post("/delete",
-    (req, res, next) => checkToken(req, res, next),
-    (req, res) => deleteReport(res, req.body));
-
 app.use((err, req, res, next) => {
     if (res.headersSent) {
         return next(err);
@@ -186,124 +67,11 @@ app.use((err, req, res, next) => {
     return undefined;
 });
 
-function checkLogin(req, res) {
-    let sql = "SELECT password FROM users WHERE email = ?";
-
-    db.get(sql, [req.body.usr], (err, row) => {
-        if (err) {
-            return res.status(403).json({
-                data: {
-                    msg: 'login failed'
-                }
-            });
-        }
-        if (row) {
-            bcrypt.compare(req.body.psw, row.password, function(err, result) {
-                if (result === true) {
-                    const jwt = require('jsonwebtoken');
-                    const payload = { email: req.body.usr };
-                    const secret = process.env.JWT_SECRET;
-                    const token = jwt.sign(payload, secret, { expiresIn: '1h'});
-
-                    return res.status(201).json({
-                        data: {
-                            token: token
-                        }
-                    });
-                }
-                return res.status(403).json({
-                    data: {
-                        msg: 'login failed'
-                    }
-                });
-            });
-            return undefined;
-        }
-        return res.status(403).json({
-            data: {
-                msg: 'login failed'
-            }
-        });
-    });
-    return undefined;
-}
-
-function checkToken(req, res, next) {
-    const jwt = require('jsonwebtoken');
-    const token = req.headers['x-access-token'];
-
-    jwt.verify(token, process.env.JWT_SECRET, function(err) {
-        if (err) {
-            return res.status(403).json({
-                data: {
-                    msg: 'not authorized jwt',
-                    jwt: err
-                }
-            });
-        }
-        next();
-        return undefined;
-    });
-}
-
-function addReport(res, body) {
-    db.run(`INSERT INTO texts (heading, content) VALUES(?,?)`,
-        [body.heading, body.content], function(err) {
-            if (err) {
-                return res.status(201).json({
-                    data: {
-                        msg: 'not authorized add'
-                    }
-                });
-            }
-            return res.status(201).json({
-                data: {
-                    msg: 'success'
-                }
-            });
-        });
-}
-
-function editReport(res, body) {
-    db.run(`UPDATE texts SET heading = ?, content = ? WHERE id = ?`,
-        [body.heading, body.content, body.id], function(err) {
-            if (err) {
-                return res.status(401).json({
-                    data: {
-                        msg: 'not authorized'
-                    }
-                });
-            }
-            return res.status(201).json({
-                data: {
-                    msg: 'success'
-                }
-            });
-        });
-}
-
-function deleteReport(res, body) {
-    db.run(`DELETE FROM texts WHERE id = ?`, [body.id], function(err) {
-        if (err) {
-            return res.status(401).json({
-                data: {
-                    msg: 'not authorized'
-                }
-            });
-        }
-        return res.status(201).json({
-            data: {
-                msg: 'success'
-            }
-        });
-    });
-}
 
 app.get("/chat", async (request, response) => {
     try {
         let res = await getOldMessages(dsn, "messages", {}, {}, 0);
 
-        console.log(res);
         response.json(res);
     } catch (err) {
         console.log(err);
@@ -345,7 +113,6 @@ async function saveMessage(message) {
             if (err) {
                 throw err;
             }
-            console.log(res);
             db.close();
         });
     });
@@ -356,7 +123,6 @@ async function saveMessage(message) {
 const server = app.listen(port);
 const io = require('socket.io')(server);
 
-io.origins(['https://me.gustavbergh.me:443']);
 
 io.on('connection', function (socket) {
     socket.on('chat message', function (message) {
